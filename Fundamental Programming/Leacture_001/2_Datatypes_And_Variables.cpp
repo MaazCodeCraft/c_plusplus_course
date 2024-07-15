57e7b858aadfaf372ac2b3b01e656acd89f32ae3
Maaz Ur Rahman <maaz4cs@gmail.com>
2024-07-15 23:00:24 +0500

    2_Datatypes_And_Variables.cpp

--- Fundamental Programming/Leacture_001/2_Datatypes_And_Variables.cpp
+++ Fundamental Programming/Leacture_001/2_Datatypes_And_Variables.cpp
@@ -4,7 +4,8 @@ using namespace std;
 
 int main()
 {
-    int a = 10;
+    // Data Types
+ /*   int a = 10;
     cout << a << endl;
 
     char ch = 'm';
@@ -20,8 +21,25 @@ int main()
     cout << bl << endl;
 
     int size = sizeof (d);
-    cout << "Size of d is:  " << size << endl;
+    cout << "Size of d is:  " << size << endl; */
 
+
+    // Variables 
+    
+    int num1 = 200;
+    int num2 = 100;
+    int num3 = 10;
+    int result = num1 * num2 * num3;
+
+    result = num1 - num2;//100
+    num2 = num1; //200
+    num2 = num3; //10
+    num3++; //11
+    result = num1 * num3; //2200
+    int num4 = 2;
+
+
+    cout << "sub of " << num4 << " and " << num3 << " is " << "\n" << result;
 
     return 0;
 }
