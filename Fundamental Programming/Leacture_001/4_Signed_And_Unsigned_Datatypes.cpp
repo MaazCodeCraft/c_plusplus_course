fdb3b7cf81a413433caf5dd278cae25b27d4533e
Maaz Ur Rahman <maaz4cs@gmail.com>
2024-07-15 22:51:58 +0500

    Add files via upload

---
+++ Fundamental Programming/Leacture_001/4_Signed_And_Unsigned_Datatypes.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    unsigned int a = 302;
+    cout << a << endl;
+
+    unsigned int a1 = -302;
+    cout << a1 << endl;
+
+    signed int b = 302;
+    cout << b << endl;
+
+    signed int b1 = -302;
+    cout << b1 << endl;
+
+    return 0;
+
+}
