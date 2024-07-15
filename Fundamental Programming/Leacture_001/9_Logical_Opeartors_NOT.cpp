6c82b2a86ddcde76957e924c2b3a6fa71d07ab43
Maaz Ur Rahman <maaz4cs@gmail.com>
2024-07-15 23:21:18 +0500

    Add files via upload

---
+++ Fundamental Programming/Leacture_001/9_Logical_Opeartors_NOT.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1;
+    int num2;
+
+    cout << "please enter 1st number \n ";
+    cin >> num1;
+
+    cout << "please enter 2nd number \n ";
+    cin >> num2;
+
+    if ( num1 != num2) {
+    cout << "both numbers are different";
+    }
+    else {
+    cout << "both numbers are equal";
+    }
+
+}
