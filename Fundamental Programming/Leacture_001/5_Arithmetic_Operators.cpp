a60759178552888758f7efd7ba1267fb6bb92e6e
Maaz Ur Rahman <maaz4cs@gmail.com>
2024-07-15 23:10:04 +0500

    Add files via upload

---
+++ Fundamental Programming/Leacture_001/5_Arithmetic_Operators.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 10 / 5;
+    cout << a << endl;
+
+    float f = 20.83 / 4;
+    cout << f << endl;
+
+    double d = 30 % 5;
+    cout << d << endl;
+
+
+    return 0;
+}
