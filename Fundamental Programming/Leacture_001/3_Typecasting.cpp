43ebde2c8715ccb2bad63ac1ff04251735e883d8
Maaz Ur Rahman <maaz4cs@gmail.com>
2024-07-15 22:44:01 +0500

    Add files via upload

---
+++ Fundamental Programming/Leacture_001/3_Typecasting.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 'x';
+    cout << a << endl;
+
+    char ch = 989;
+    cout << ch << endl;
+
+    char ch1 = 123456;
+    cout << ch1 << endl;
+
+    return 0;
+}
