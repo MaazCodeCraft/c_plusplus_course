6c06c9bd062f742804c434ad77b52b0ef005d50b
Maaz Ur Rahman <maaz4cs@gmail.com>
2024-07-15 22:39:51 +0500

    Add files via upload

---
+++ Fundamental Programming/Leacture_001/2_Datatypes_And_Variables.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 10;
+    cout << a << endl;
+
+    char ch = 'm';
+    cout << ch << endl;
+
+    float f = 10.4;
+    cout << f << endl;
+
+    double d = 100.383;
+    cout << d << endl;
+
+    bool bl = true;
+    cout << bl << endl;
+
+    int size = sizeof (d);
+    cout << "Size of d is:  " << size << endl;
+
+
+    return 0;
+}
