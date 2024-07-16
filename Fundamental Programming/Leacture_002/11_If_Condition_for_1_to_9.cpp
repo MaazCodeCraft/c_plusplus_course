44d9efbb652d074b1d0482351bcdd0b632f1f459
Maaz Ur Rahman <maaz4cs@gmail.com>
2024-07-17 00:16:07 +0500

    Add files via upload

---
+++ Fundamental Programming/Leacture_002/11_If_Condition_for_1_to_9.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num ;
+    cout << "please enter 1st number \n";
+    cin >> num;
+
+    if (num>0 && num<10);
+
+    cout << "you have enter";
+
+    if (num==1){
+    cout <<" ONE";
+    }
+
+   else if (num==2){
+    cout <<" TWO";
+    }
+
+   else if (num==3){
+    cout <<" THREE";
+    }
+
+   else if (num==4){
+    cout <<" FOUR";
+    }
+
+  else  if (num==5){
+    cout <<" FIVE";
+    }
+
+  else  if (num==6){
+    cout <<" SIX";
+    }
+
+   else if (num==7){
+    cout <<" SEVEN";
+    }
+
+    else if (num==8){
+    cout <<" EIGHT";
+    }
+
+    else if (num==9){
+    cout <<" NINE";
+    }
+
+    else {
+    cout <<" wrong number";
+    }
+
+    return 0;
+}
