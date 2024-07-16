bce4c6c97885f9a3dae01de21d14f601e214bc16
Maaz Ur Rahman <maaz4cs@gmail.com>
2024-07-17 00:45:41 +0500

    Add files via upload

---
+++ Fundamental Programming/Leacture_003/10_Simple_Calculator.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1, num2;
+    int sum, difference, product, division, remainder, average;
+
+    cout << "enter 1st number" << endl;
+    cin >> num1;
+
+    cout << "enter 2nd number" << endl;
+    cin >> num2;
+
+
+    sum = num1 + num2;
+    cout << "sum of " << num1 << " and " << num2 << " is " << sum << endl;
+
+    difference = num1 - num2;
+    cout << "difference of " << num1 << " and " << num2 << " is " << difference << endl;
+
+    product = num1 * num2;
+    cout << "product of " << num1 << " and " << num2 << " is " << product << endl;
+
+    division = num1 / num2;
+    cout << "division of " << num1 << " and " << num2 << " is " << division << endl;
+
+    remainder = num1 % num2;
+    cout << "remainder of " << num1 << " and " << num2 << " is " << remainder << endl;
+
+    return 0;
+}
