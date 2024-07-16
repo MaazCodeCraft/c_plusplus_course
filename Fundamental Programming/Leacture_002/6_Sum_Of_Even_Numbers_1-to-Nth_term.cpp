748e9f9b361acdf566639e82cf657bfbdb8a50f4
Maaz Ur Rahman <maaz4cs@gmail.com>
2024-07-16 23:54:55 +0500

    Add files via upload

---
+++ Fundamental Programming/Leacture_002/6_Sum_Of_Even_Numbers_1-to-Nth_term.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int n;
+
+    cout << "Enter the value of nth : ";
+    cin >> n;
+
+    int i = 2;
+    int sum = 0;
+
+    while(i <= n) {
+        sum = sum + i;
+        i = i + 2;
+    }
+
+    cout << "Sum of even numbers upto nth =  " << sum << endl;
+
+    return 0;
+}
