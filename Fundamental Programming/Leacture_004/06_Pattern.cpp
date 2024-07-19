04dab6f78f6f278404ec265dc8cdbfe6ee9d8f86
Maaz Ur Rahman <maaz4cs@gmail.com>
2024-07-19 21:31:19 +0500

    Add files via upload

---
+++ Fundamental Programming/Leacture_004/06_Pattern.cpp
@@ -0,0 +1,29 @@
+/*
+    1
+    2 3
+    4 5 6
+    7 8 9 10
+    */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num;
+
+    cout << "Enter the value of n: ";
+    cin >> num;
+
+    int count = 1;
+
+    for (int row = 1; row <= num; row++) {
+        for (int col = 1; col <= row; col++) {
+            cout << count << " ";
+            count++;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
