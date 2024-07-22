3988caf9700949a00b0b86000e27a6c3000ebcf8
Maaz Ur Rahman <maaz4cs@gmail.com>
2024-07-22 12:18:44 +0500

    Add files via upload

---
+++ Fundamental Programming/Leacture_005/11_Pattern.cpp
@@ -0,0 +1,29 @@
+/*
+    *
+    **
+    ***
+    ****
+    *****
+
+    */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num;
+    cout << "Enter the Value of n:  ";
+    cin >> num;
+
+    for (int i = 1; i <= num; i++)
+    {
+        for (int j = 1; j <=  i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
