09a79bef64201ec6ab3222d70f4d735b7423aafa
Maaz Ur Rahman <maaz4cs@gmail.com>
2024-07-22 12:35:45 +0500

    Add files via upload

---
+++ Fundamental Programming/Leacture_005/17_Pattern_Diamond.cpp
@@ -0,0 +1,54 @@
+    /*
+
+    *
+   ***
+  *****
+ *******
+*********
+*********
+ *******
+  *****
+   ***
+    *
+
+    */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num;
+    cout << "Enter the Value of n:  ";
+    cin >> num;
+
+    //Upper Part
+    for (int i = 1; i <= num; i++)
+    {
+        for (int j = 1; j <=  num - i; j++)
+        {
+            cout << " ";
+        }
+        for (int k = 1; k <= 2 * i - 1; k++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+     //Lower Part
+    for (int i = num; i >= 1; i--)
+    {
+        for (int j = 1; j <=  num - i; j++)
+        {
+            cout << " ";
+        }
+        for (int k = 1; k <= 2 * i - 1; k++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
