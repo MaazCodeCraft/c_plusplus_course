e34f942158b6fd2c1c65389e4fb8049f7e1e70eb
Maaz Ur Rahman <maaz4cs@gmail.com>
2024-07-23 12:31:55 +0500

    Add files via upload

---
+++ Fundamental Programming/Leacture_006/02_While_Loop.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i = 1;
+
+    while (i <= 100)
+    {
+        cout << i;
+        i++;
+        cout << endl;
+
+    }
+
+    return 0;
+}
