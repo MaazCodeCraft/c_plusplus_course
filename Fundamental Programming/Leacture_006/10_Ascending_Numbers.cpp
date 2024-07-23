3556221d8cfe3721bfae698ed66d359d2019d4a6
Maaz Ur Rahman <maaz4cs@gmail.com>
2024-07-23 12:38:12 +0500

    Add files via upload

---
+++ Fundamental Programming/Leacture_006/10_Ascending_Numbers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num;
+    cout << "Enter a Number:  ";
+    cin >> num;
+
+    for (int i = 1; i <= num; i++)
+    {
+        cout << i << " ";
+    }
+
+    return 0;
+}
