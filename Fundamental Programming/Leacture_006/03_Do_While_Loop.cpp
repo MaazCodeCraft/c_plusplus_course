4910aa65b8c15e6a47cb6f40ae11f8d189b567cd
Maaz Ur Rahman <maaz4cs@gmail.com>
2024-07-23 12:32:20 +0500

    Add files via upload

---
+++ Fundamental Programming/Leacture_006/03_Do_While_Loop.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i = 1;
+
+    do {
+        cout << "*";
+        i++;
+        cout << endl;
+    }
+    while (i <= 5);
+
+    return 0;
+}
